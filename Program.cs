2deab5ada7d4310df992ebc2892f94708275958b
João Santos <131533011+JoaosantosL@users.noreply.github.com>
2024-01-03 20:11:46 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arrayado = { 1, 2, 3, 4, 5 };
+        Span<int> span = array.AsSpan();
+
+        span[1] = 10; // pq é um a mais ou a menos ahco eu
+
+        foreach (int numero in span)
+        {
+            Console.WriteLine(numero);
+        }
+        Console.WriteLine("Se isto funcionar batam palmas");
+    }
+    
+}
